980a382102156930001ecc8e14ba45904c4289f2
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-02-27 21:13:07 +0900

    Update Intermediate Flow Operators

--- src/main/kotlin/a05_intermediateflowoperators/Main.kt
+++ src/main/kotlin/a05_intermediateflowoperators/Main.kt
@@ -1,14 +1,39 @@
 package a05_intermediateflowoperators
 
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 
-suspend fun performRequest(request: Int): String {
-    return "response $request"
-}
+
 
 fun main() = runBlocking {
+
+    // Map を利用した例
+    suspend fun performRequest(request: Int): String {
+        return "response $request"
+    }
+
     (1..3).asFlow().map { request -> performRequest(request) }.collect { response -> println(response) }
+
+    // Transform を利用した例
+    (1..3).asFlow().transform { request ->
+        emit("Making request $request")
+        emit(performRequest(request))
+    }.collect { response ->
+        println(response)
+    }
+
+    // Take を利用した例
+    val takeFlow = flow {
+        try {
+            emit(1)
+            emit(2)
+            emit(3)
+        } catch (e: Exception) {
+            println(e.message)
+        } finally {
+            println("Finally in numbers")
+        }
+    }
+
+    takeFlow.take(2).collect { value -> println(value) }
 }
