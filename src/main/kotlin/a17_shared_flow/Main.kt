82dfa345d753a69362bdf894e88f02bd7996da2a
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-01-20 23:44:29 +0900

    feat: update shared_flow test

--- src/main/kotlin/a17_shared_flow/Main.kt
+++ src/main/kotlin/a17_shared_flow/Main.kt
@@ -1,24 +1,58 @@
 package a17_shared_flow
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.*
+import java.util.*
 
 fun main() = runBlocking<Unit> {
-    // Replay テスト
-    launch {
-        // collect するとそこでコルーチンの処理が止まるので別々のコルーチンで実行してやる
-        println("replayTestStart: emitCount 10 replayCount 1")
-        replayTest(emitCount = 10, replayCount = 1)
-    }
-    launch {
-        // collect するとそこでコルーチンの処理が止まるので別々のコルーチンで実行してやる
-        println("replayTestStart: emitCount 10 replayCount 5")
-        replayTest(emitCount = 10, replayCount = 5)
-    }
-    launch {
-        // collect するとそこでコルーチンの処理が止まるので別々のコルーチンで実行してやる
-        println("replayTestStart: emitCount 10 replayCount 10")
-        replayTest(emitCount = 10, replayCount = 10)
+//    println("start")
+//    defaultTest()
+
+    println("start: emitCount 10 bufferOption BufferOverflow.SUSPEND")
+    bufferTest(10, replayCount = 1, BufferOverflow.SUSPEND)
+
+    println("start: emitCount 10 bufferOption BufferOverflow.DROP_LATEST")
+    bufferTest(10, replayCount = 5, BufferOverflow.DROP_LATEST)
+
+    println("start: emitCount 10 bufferOption BufferOverflow.DROP_OLDEST")
+    bufferTest(10, replayCount = 5, BufferOverflow.DROP_OLDEST)
+
+//    println("start: emitCount 10 replayCount 0")
+//    replayTest(emitCount = 10, replayCount = 0)
+//
+//    println("start: emitCount 10 replayCount 1")
+//    replayTest(emitCount = 10, replayCount = 1)
+//
+//    println("start: emitCount 10 replayCount 5")
+//    replayTest(emitCount = 10, replayCount = 5)
+//
+//    println("start: emitCount 10 replayCount 10")
+//    replayTest(emitCount = 10, replayCount = 10)
+}
+
+/**
+ * SharedFlow の基本的な動作のテスト
+ */
+fun defaultTest() {
+    runBlocking {
+        val mutableSharedFlow = MutableSharedFlow<Int>()
+
+        println("emit:0")
+        mutableSharedFlow.emit(0)
+
+        delay(1000)
+
+        mutableSharedFlow.onEach {
+            println("collect:${it}")
+        }.launchIn(GlobalScope)
+
+        delay(1000)
+
+        println("emit:1")
+        mutableSharedFlow.emit(1)
+
+        delay(1000)
     }
 }
 
@@ -27,14 +61,52 @@ fun main() = runBlocking<Unit> {
  * emitCount: エミットする値の数
  * replayCount: リプライする値の数
  */
-suspend fun replayTest(emitCount: Int, replayCount: Int){
-    val mutableSharedFlow: MutableSharedFlow<Int> = MutableSharedFlow(replay = replayCount)
-    for (i in 1..emitCount) {
-        println("emit:${i}")
-        mutableSharedFlow.emit(i)
-    }
+fun replayTest(emitCount: Int, replayCount: Int){
+    runBlocking {
+        val mutableSharedFlow = MutableSharedFlow<Int>(replay = replayCount)
 
-    val sharedFlow: SharedFlow<Int> = mutableSharedFlow.asSharedFlow()
-    sharedFlow.onEach { println("collect:${it}") }.collect()
+        for (i in 1..emitCount) {
+            println("emit:${i}")
+            mutableSharedFlow.emit(i)
+        }
+
+        mutableSharedFlow.onEach {
+            println("collect:${it}")
+        }.launchIn(GlobalScope)
+
+        // 値のコレクトが完了するまで待つ
+        delay(1000)
+    }
+}
+
+/**
+ * Replay の動作を確認するテスト
+ * emitCount: エミットする値の数
+ * replayCount: リプライする値の数
+ */
+fun bufferTest(emitCount: Int, replayCount: Int, bufferOverflow: BufferOverflow) {
+    runBlocking {
+        val mutableSharedFlow = MutableSharedFlow<Int>(replay = replayCount, onBufferOverflow = bufferOverflow)
+
+        mutableSharedFlow.onEach {
+            delay(100)
+            println("slow:collect: ${it}")
+        }.launchIn(GlobalScope)
+
+        mutableSharedFlow.onEach {
+            println("collect: ${it}")
+        }.launchIn(GlobalScope)
+
+        delay(1000)
+
+        for (i in 1..emitCount) {
+            println("emit: value ${i}")x
+            mutableSharedFlow.emit(i)
+            println("emitted: value ${i}")
+        }
+
+        // 値のコレクトが完了するまで待つ
+        delay(1000)
+    }
 }
 
--- src/main/kotlin/a17_shared_flow/README.md
+++ src/main/kotlin/a17_shared_flow/README.md
@@ -26,3 +26,102 @@
 # SharedFlow vs BroadcastChannel
 
 - SharedFlow は BroadcastChannel の代替として使える
+
+
+# SharedFlow の Replay の動作について
+
+```kotlin
+/**
+ * Replay の動作を確認するテスト
+ * emitCount: エミットする値の数
+ * replayCount: リプライする値の数
+ */
+fun replayTest(emitCount: Int, replayCount: Int){
+  runBlocking {
+    // MutableSharedFlow を生成する
+    val mutableSharedFlow = MutableSharedFlow<Int>(replay = replayCount)
+
+    // MutableSharedFlow で値をエミットする
+    for (i in 1..emitCount) {
+      println("emit:${i}")
+      mutableSharedFlow.emit(i)
+    }
+
+    // MutableSharedFlow で値をコレクトする
+    mutableSharedFlow.onEach {
+      println("collect:${it}")
+    }.launchIn(GlobalScope)
+
+    // 値のコレクトが完了するまで待つ
+    delay(1000)
+  }
+}
+```
+
+- Replay を設定すると古い値をキャッシュすることができる
+- Collect したときにキャッシュされた値があれば受信される
+- Replay を 0 に設定したときは Collect される前の値は受信できない
+- Replay を 5 に設定したときは Collect される前の値を5つ受信できる
+
+```
+start: emitCount 10 replayCount 0
+emit:1
+emit:2
+emit:3
+emit:4
+emit:5
+emit:6
+emit:7
+emit:8
+emit:9
+emit:10
+
+start: emitCount 10 replayCount 1
+emit:1
+emit:2
+emit:3
+emit:4
+emit:5
+emit:6
+emit:7
+emit:8
+emit:9
+emit:10
+collect:10
+start: emitCount 10 replayCount 5
+emit:1
+emit:2
+emit:3
+emit:4
+emit:5
+emit:6
+emit:7
+emit:8
+emit:9
+emit:10
+collect:6
+collect:7
+collect:8
+collect:9
+collect:10
+start: emitCount 10 replayCount 10
+emit:1
+emit:2
+emit:3
+emit:4
+emit:5
+emit:6
+emit:7
+emit:8
+emit:9
+emit:10
+collect:1
+collect:2
+collect:3
+collect:4
+collect:5
+collect:6
+collect:7
+collect:8
+collect:9
+```
