e6c3a532e96584e4ec72d6819a4790ed27b87435
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-01-25 21:48:41 +0900

    feat: add state flow sample

---
+++ src/main/kotlin/a18_state_flow/Main.kt
@@ -0,0 +1,10 @@
+package a18_state_flow
+
+import a17_shared_flow.bufferTest
+import a17_shared_flow.defaultTest
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking<Unit> {
+    println("Hello World!!")
+}
