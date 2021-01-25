5a4a9deced5f954e26292f707e5c5904f2e33861
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-01-25 23:45:17 +0900

    feat: update state flow sample

--- src/main/kotlin/a18_state_flow/Main.kt
+++ src/main/kotlin/a18_state_flow/Main.kt
@@ -1,10 +1,112 @@
 package a18_state_flow
 
-import a17_shared_flow.bufferTest
-import a17_shared_flow.defaultTest
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking<Unit> {
-    println("Hello World!!")
+    distinctTest2()
+}
+
+/**
+ *  StateFlow の初期値のテスト
+ */
+fun initialValueTest() {
+    runBlocking {
+        val mutableStateFlow = MutableStateFlow<Int>(value = 0)
+
+        mutableStateFlow.onEach {
+            println("collect:${it}")
+        }.launchIn(GlobalScope)
+
+        delay(1000)
+
+        println("emit:1")
+        mutableStateFlow.emit(1)
+
+        delay(1000)
+    }
+}
+
+fun emitTest() {
+    runBlocking {
+        val mutableStateFlow = MutableStateFlow<Int>(value = 0)
+
+        mutableStateFlow.onEach {
+            println("collect:${it}")
+        }.launchIn(GlobalScope)
+
+        delay(1000)
+
+        println("emit:1")
+        mutableStateFlow.emit(1)
+
+        delay(1000)
+    }
+}
+
+fun emitTest2() {
+    runBlocking {
+        val mutableStateFlow = MutableStateFlow<Int>(value = 0)
+
+        println("emit:1")
+        mutableStateFlow.emit(1)
+
+        delay(1000)
+
+        mutableStateFlow.onEach {
+            println("collect:${it}")
+        }.launchIn(GlobalScope)
+
+        delay(1000)
+
+        println("emit:2")
+        mutableStateFlow.emit(2)
+
+        delay(1000)
+    }
+}
+
+fun distinctTest() {
+    runBlocking {
+        val mutableStateFlow = MutableStateFlow<Int>(value = 0)
+
+        mutableStateFlow.onEach {
+            println("collect:${it}")
+        }.launchIn(GlobalScope)
+
+        delay(1000)
+
+        println("emit:0")
+        mutableStateFlow.emit(0)
+
+        delay(1000)
+    }
+}
+
+fun distinctTest2() {
+    runBlocking {
+        val mutableStateFlow = MutableStateFlow<Int>(value = 0)
+
+        println("emit:1")
+        mutableStateFlow.emit(1)
+
+        delay(1000)
+
+        mutableStateFlow.onEach {
+            println("collect:${it}")
+        }.launchIn(GlobalScope)
+
+        delay(1000)
+
+        println("emit:1")
+        mutableStateFlow.emit(1)
+
+        delay(1000)
+    }
 }
